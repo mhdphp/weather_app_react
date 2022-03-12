665db0487e5d194d03e1dd5826966f0e3dc1275c
Mihai Enuica <mihai.enuica@gmail.com>
2022-03-12 12:10:17 +0200

    Try catch and manual convertion of K into Celsius

--- src/App.js
+++ src/App.js
@@ -29,22 +29,43 @@ function App() {
   }, []);
 
   // https://home.openweathermap.org/api_keys
-  const fetchData = async() => {
-    const APIKEY = '73cd9a5978e2edb36b1237edec6cf8c9';
-    const city = 'Atlanta';
-    // axios is a library which will allow to make requests to the server / database
-    const result = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKEY}`);
-    await setAllData({
-      city:result.data.name,
-      country: result.data.sys.country,
-      temperature:result.data.main.temp
-    });
+  const fetchData = async(city) => {
+
+    try {
+      const APIKEY = '73cd9a5978e2edb36b1237edec6cf8c9';
+      const city = 'Atlanta'; // for testing purposes
+      // axios is a library which will allow to make requests to the server / database
+      const result = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKEY}`);
+
+      let Ctemp = Math.round(result.data.main.temp - 273.15); // conver temperature to Celsius
+      Ctemp = String(Ctemp) + "C"; // convert Ctemp to string
+
+      await setAllData({
+        city:result.data.name,
+        country: result.data.sys.country,
+        temperature:Ctemp
+      });  
+    } catch (error) {
+      console.log('API not loaded correctly or loaded for the first time.');
+    }
   }
 
   return (
-    <div className="App">
-      {console.log('This is a test', allData.country, allData.temperature)}
-    </div>
+    // section tag in react
+    // main tag for the main build
+    <main>
+      <div className="App">
+            {console.log('This is a test', 
+                  allData.city, allData.country, allData.temperature)}
+        <section>
+          <h1>{allData.city}</h1>
+          <h2>{allData.country}</h2>
+          <h3>Temperature</h3>
+          <h4>{allData.temperature}</h4>
+        </section>
+      </div>
+      
+    </main>
   );
 }
 
